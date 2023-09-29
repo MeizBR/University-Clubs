9c20ed0b01f53b4b0b690f522189de80bb2b67a5
MeizBR <mayezbr9@gmail.com>
2023-09-29 22:41:21 +0100

    display-add-update and delete users routes

--- index.js
+++ index.js
@@ -1,18 +1,16 @@
-// Import the Express.js module
 const express = require('express');
 
-// Create an instance of the Express application
 const app = express();
 
-// app.use(cors());
+const cors = require('cors');
+
+app.use(cors());
+
+app.use(express.json());
 
 const pool = require('./DB/db_connection');
 
-// Define a route that responds with "Hello, Express!" when accessed
-app.get('/', (req, res) => {
-  res.send('Hello, Express!');
-});
-
+// Display users
 app.get('/users', (req, res) => {
   pool.query('SELECT * FROM users', (error, result) => {
     if (error) {
@@ -24,8 +22,72 @@ app.get('/users', (req, res) => {
   });
 });
 
+// Add user
+app.post('/add-user', async(req, res) => {
+  try {
+    const { firstname, lastname, age, job } = req.body;
 
-// Start the Express server and listen on port 5000
+    const query = `
+      INSERT INTO users (firstname, lastname, age, job)
+      VALUES ($1, $2, $3, $4)
+      RETURNING *;`;
+
+    const values = [firstname, lastname, age, job];
+
+    const result = await pool.query(query, values);
+
+    res.status(201).json(result.rows[0]);
+  } catch (error) {
+    console.error('Error adding user:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+// Update a user by ID
+app.put('/update-user/:id', async (req, res) => {
+  const userId = req.params.id;
+  const { firstname, lastname, age, job } = req.body;
+
+  try {
+    const query = `
+      update users
+      set firstname = $1, lastname = $2, age = $3, job = $4
+      where id = $5
+      RETURNING *;`;
+
+    const values = [firstname, lastname, age, job, userId];
+
+    const result = await pool.query(query, values);
+
+    res.status(201).json(result.rows[0]);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+// Delete a user by ID
+app.delete('/delete-user/:id', async (req, res) => {
+  const userId = req.params.id;
+
+  try {
+    const client = await pool.connect();
+    const deleteQuery = 'DELETE FROM users WHERE id = $1';
+    const result = await client.query(deleteQuery, [userId]);
+    client.release();
+
+    if (result.rowCount === 1) {
+      res.json({ message: 'User deleted successfully' });
+    } else {
+      res.status(404).json({ error: 'User not found' });
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+
 app.listen(5000, () => {
   console.log('Server is running on http://localhost:5000');
 });
